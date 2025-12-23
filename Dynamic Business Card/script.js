5106b42690f9a633c3922c235ba56d4deceb957c
RenatoLPA <renatolucaspandrade@gmail.com>
2025-12-23 17:51:49 -0300

    Dynamic Business Card

--- Dynamic Business Card/script.js
+++ Dynamic Business Card/script.js
@@ -3,7 +3,18 @@ function inserirInfos() {
     let profissao = document.getElementById("profissa").value 
     let idade = document.getElementById("age").value 
 
+    let sexoSelecionado = ''
+
+    if (document.getElementById("masc").checked) {
+        sexoSelecionado = "Masculino"
+    } if (document.getElementById("fem").checked) {
+        sexoSelecionado = "Feminino"
+    } if (document.getElementById("other").checked) {
+        sexoSelecionado = "Outro"
+    }
+
     document.getElementById("nome").innerHTML = nome 
     document.getElementById("prof").innerHTML = profissao 
     document.getElementById("idade").innerHTML = idade
+    document.getElementById("sexo").innerText = sexoSelecionado
 }
